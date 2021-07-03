5197da9cbf053d331e27ccbeb7ae398e2e25da90
unknown <pfspring84@gmail.com>
2021-07-02 23:01:31 -0500

    friday changes - ability to pick off change

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -10,9 +10,9 @@ function initialLoad() {
 		console.log('made it here');
   
 		// Slice the first 10 objects for plotting
-		console.log(data.samples[0].sample_values.slice(0,9));
+		console.log(data.samples[0].sample_values.slice(0,10));
 
-		firstTenSamples = data.samples[0].sample_values.slice(0,9);
+		firstTenSamples = data.samples[0].sample_values.slice(0,10);
 		//console.log('made it here too');
   
 		// Reverse the array due to Plotly's defaults
@@ -20,14 +20,12 @@ function initialLoad() {
 		console.log(firstTenSamples);
 		//console.log('made it here toox2');
 	
-		firstTenSampleIds = data.samples[0].otu_ids.slice(0,9)
+		firstTenSampleIds = data.samples[0].otu_ids.slice(0,10)
 		firstTenSampleIds = firstTenSampleIds.reverse();
-		firstTenSampleLabels = data.samples[0].otu_labels.slice(0,9)
+		firstTenSampleLabels = data.samples[0].otu_labels.slice(0,10)
 		firstTenSampleLabels = firstTenSampleLabels.reverse();
 		//console.log(firstTenSampleIds);
 		//console.log('made it here toox3');
-		//var strArr = firstTenSampleIds.map(String);
-		//console.log(strArr);
 
 		var trace1 = {
 		  x: firstTenSamples.map(row => row),
@@ -66,6 +64,66 @@ function initialLoad() {
 		  .text(function (d) { return d; }) // text showed in the menu
 		  .attr("value", function (d) { return d; }) // corresponding value returned by the button
 	
+	});
+};
+
+function optionChanged(selectedSample) {
+	//console.log(selectedSample);
+	d3.json("samples.json").then((importedData) => {
+		var data = importedData;
+		//console.log(data.names);
+		for (let i = 0; i < (data.names).length; i++) {
+			if (data.names[i] == selectedSample) sampleKey = i;
+		}
+		console.log(sampleKey);
+
+		// Slice the first 10 objects for plotting
+		//console.log(data.samples[sampleKey].sample_values.slice(0,9));
+
+		firstTenSamples = data.samples[sampleKey].sample_values.slice(0,10);
+		//console.log('made it here too');
+  
+		// Reverse the array due to Plotly's defaults
+		firstTenSamples = firstTenSamples.reverse();
+		console.log(firstTenSamples);
+		//console.log('made it here toox2');
+	
+		firstTenSampleIds = data.samples[sampleKey].otu_ids.slice(0,10)
+		firstTenSampleIds = firstTenSampleIds.reverse();
+		firstTenSampleLabels = data.samples[sampleKey].otu_labels.slice(0,10)
+		firstTenSampleLabels = firstTenSampleLabels.reverse();
+		//console.log(firstTenSampleIds);
+		//console.log('made it here toox3');
+
+		var trace1 = {
+		  x: firstTenSamples.map(row => row),
+		  y: firstTenSampleIds.map(row => row),
+		  text: firstTenSampleLabels.map(row => row),
+		  type: "bar",
+		  orientation: "h"
+		};
+  
+		// data
+		var chartData = [trace1];
+  
+		// Apply the group bar mode to the layout
+		var layout = {
+		  title: "Top 10 OTUs for Selected Individual",
+		  yaxis: {
+			type: 'category',
+			dtick: 1
+		  },			  	
+		  margin: {
+			l: 100,
+			r: 100,
+			t: 100,
+			b: 100
+		  }
+		};
+  
+		// Render the plot to the div tag with id "plot"
+		Plotly.newPlot("bar", chartData, layout);
+
 	});
 };
 
